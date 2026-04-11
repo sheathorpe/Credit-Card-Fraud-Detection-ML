52b9d33df63b5a75d920361a9d0b8cd5e849a7a1
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-04-10 21:26:45 -0400

    added code for training XGBoost classifer, dropped accuracy by ~30%, increased recall to ~47%

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from imblearn.over_sampling import SMOTE
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.metrics import recall_score, precision_score, roc_auc_score, accuracy_score
 from preprocessing import transform_data
 
@@ -34,7 +34,32 @@ precision = precision_score(y_test, y_pred_new)
 acc = accuracy_score(y_test, y_pred_new)
 
 # printing metrics
+print('Random Forest Performance Metrics')
 print(f'Accuracy: {acc}')
 print(f'ROC AUC: {roc_auc}')
 print(f'Recall: {recall}')
 print(f'Precision: {precision:}')
+
+# Initialize XGBoost model
+xgb_model = HistGradientBoostingClassifier(
+    max_iter=100,
+    random_state=42
+)
+
+xgb_model.fit(X=X_train_res, y=y_train_res)
+y_predict = xgb_model.predict(X_test)
+y_probs = xgb_model.predict_proba(X_test)[:, 1]
+y_pred_new = (y_probs > 0.1).astype(int)
+
+# calculating performance metrics
+roc_auc = roc_auc_score(y_test, y_probs)
+recall = recall_score(y_test, y_pred_new)
+precision = precision_score(y_test, y_pred_new)
+acc = accuracy_score(y_test, y_pred_new)
+
+# printing metrics
+print('\nXGBoost Performance Metrics')
+print(f'Accuracy: {acc}')
+print(f'ROC AUC: {roc_auc}')
+print(f'Recall: {recall}')
+print(f'Precision: {precision}')
